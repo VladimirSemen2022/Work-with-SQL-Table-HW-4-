6f790e84d5ce75d9551e00207f023bc775f08b5c
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2022-07-23 17:08:54 +0300

    Created new Singleton class - "Link" with linkLine  connection to SQL-base from file

---
+++ Work with SQL-Table (HW-4)/Link.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_with_SQL_Table__HW_4_
+{
+    public sealed class Link
+    {
+        static string link;
+        public SqlConnection connection { get; private set; }
+
+        private Link()     //Создание подключения к базе SQL
+        {
+            connection = new SqlConnection(link);
+            connection.Open();      //Открытие соединения с базой SQL
+        }
+
+        private static Link _instance;     //Внутренняя переменная класса, хранящая соединение с базой 
+        public static Link GetInstance(string fileName = "Link.txt")     //Метод создания подключения к базе SQL
+        {
+            if (_instance == null)
+            {
+                link = File.ReadAllText(fileName);
+                _instance = new Link();
+            }
+            return _instance;
+        }
+
+        public ObjectLog[] ReadAndShow(bool show)  //Если параметр show при обращении к методу равен true то кроме чтения базы, данные также выведутся на экран
+        {
+            ObjectLog[] Table = new ObjectLog[1];
+            using (SqlCommand cmd = new SqlCommand("SELECT TOP (1000) [id],[Name],[SoldierType],[WeaponType],[HP],[Action],[DateTime]FROM[LogFile].[dbo].[Log]", connection))
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                int i = 0;
+                while (reader.Read())
+                {
+                    Table[i] = new ObjectLog(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetInt32(4), reader.GetString(5), reader.GetDateTime(6));
+                    Array.Resize(ref Table, Table.Length + 1);
+                    if (show) Console.WriteLine(Table[i].ToString());
+                    i++;
+                }
+                reader.Close();
+                if (show) Console.WriteLine($"From the table read {i} rows");
+            }
+            return Table;
+        }
+
+        public void Add(ObjectLog Table)      //Метод записи данных в открытую базу SQL. Данные передаются в виде единичного объекта класса ObjectLog
+        {
+            if (Table != null)
+            {
+                string sqlText = String.Format($@"INSERT INTO [dbo].[Log]([Name],[SoldierType],[WeaponType],[HP],[Action],[DateTime]) VALUES ('{Table.Name}','{Table.Type}', '{Table.Weapone}', {Table.HP}, '{Table.Action}', '{Table.DateTime.ToString("yyyy-MM-dd HH:mm:ss")}');");
+                SqlCommand command = new SqlCommand(sqlText, connection);
+                Console.WriteLine($"To the table Log added {command.ExecuteNonQuery()} row");
+            }
+        }
+
+        public void Add(ObjectLog[] Table)      //Метод записи данных в открытую базу SQL. Данные передаются в виде массива данных класса ObjectLog
+        {
+            if (Table != null)
+            {
+                int i = 0;
+                foreach (ObjectLog item in Table)
+                {
+                    if (item != null)
+                    {
+                        string sqlText = String.Format($@"INSERT INTO [dbo].[Log]([Name],[SoldierType],[WeaponType],[HP],[Action],[DateTime]) VALUES ('{item.Name}','{item.Type}', '{item.Weapone}', {item.HP}, '{item.Action}', '{item.DateTime.ToString("yyyy-MM-dd HH:mm:ss")}');");
+                        SqlCommand command = new SqlCommand(sqlText, connection);
+                        i += command.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine($"To the table Log added {i} rows");
+            }
+        }
+
+        public void Delete(int col)         //Удаление указанного в параметре col количества строк
+        {
+            if (ReadAndShow(false).Length > 1)
+            {
+                string sqlText = $"DELETE TOP ({col}) FROM [dbo].[Log]";
+                SqlCommand command = new SqlCommand(sqlText, connection);
+                Console.WriteLine($"From table Log deleted {command.ExecuteNonQuery()} rows");
+
+            }
+        }
+
+        public void Delete(string nameCol, string value)        //Удаление строк с указанным в параметрах значением value для столбца с именем nameCol
+        {
+            if (ReadAndShow(false).Length > 1)
+            {
+                string sqlText = $"DELETE FROM [dbo].[Log] WHERE {nameCol}='{value}'";
+                SqlCommand command = new SqlCommand(sqlText, connection);
+                Console.WriteLine($"In the column {nameCol}={value} the table Log was found and deleted {command.ExecuteNonQuery()} rows");
+            }
+        }
+    }
+}
--- Work with SQL-Table (HW-4)/Program.cs
+++ Work with SQL-Table (HW-4)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Work_with_SQL_Table__HW_4_
 {
@@ -7,13 +8,25 @@ namespace Work_with_SQL_Table__HW_4_
         static void Main(string[] args)
         {
             //Создание класса Singleton (одиночка) для реализации подключения к базе данных SQL с наличием методов чтения, удаления, добавления
-            SQLTable SQL_obj = SQLTable.GetInstance("Server=DESKTOP-MV43C0T;Database=LogFile;Trusted_Connection=True;");
-            
-            ObjectLog[] Table1 = SQL_obj.ReadAndShow(true);
-            SQL_obj.Delete("Action", "moves");
-            SQL_obj.ReadAndShow(true);
-            SQL_obj.Add(Table1);
-            SQL_obj.ReadAndShow(true);
+            //const string fileName = "Link.txt";
+            //if (!File.Exists(fileName))
+            //    File.WriteAllText(fileName, "Server=DESKTOP-MV43C0T;Database=LogFile;Trusted_Connection=True;");
+
+            //string link = File.ReadAllText(fileName);
+            //SQLTable.GetInstance(link);
+
+
+            //Singleton класс с подключением к базе SQL из строки, сохраненной в файле
+            //по умолчанию класс получает строку подключения из байла "Link.txt" или можно указать в параметрах
+            //название или полный путь к файлу, хранящему строку подключения к SQL базе
+            Link.GetInstance();
+
+            //ObjectLog[] Table1 = Link.GetInstance().ReadAndShow(true);
+            Link.GetInstance().ReadAndShow(true);
+            Link.GetInstance().Delete("Name", "Ruf");
+            Link.GetInstance().ReadAndShow(true);
+            //Link.GetInstance().Add(Table1);
+            //Link.GetInstance().ReadAndShow(true);
         }
     }
 }
